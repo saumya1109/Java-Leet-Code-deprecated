22a37547555dc852e09231b4b61a9b8e34e30c72
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-06-22 12:52:17 -0400

    Adding the optimal method

--- TwoSums/src/Main.java
+++ TwoSums/src/Main.java
@@ -3,10 +3,10 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {3, 2, 4};
-/*        int[] array = {3, 3};
-        int[] array = {2, 7, 9,14};*/
-        int target = 6;
+        //int[] array = {3, 2, 4};
+        //int[] array = {3, 3};
+       int[] array = {2, 7, 9,14};
+        int target = 9;
 
         //Brute Force Approach
 
@@ -22,6 +22,9 @@ public class Main {
         int[] output1 = twoSumsComplement(array, target);
         System.out.println(output1[0] + " " + output1[1]);
 
+        int[] output2 = twoSumsOnePass(array, target);
+        System.out.println(output1[0] + " " + output1[1]);
+
     }
 
     public static int[] twoSumsBF(int[] array, int target) {
@@ -29,6 +32,7 @@ public class Main {
         for (int i = 0; i < length - 1; i++) {
             for (int j = i + 1; j < length; j++) {
                 if (array[i] + array[j] == target) {
+
                     return new int[]{i, j};
                 }
 
@@ -50,6 +54,21 @@ public class Main {
                 return new int[]{i, map.get(complement)};
             }
 
+        }
+        return new int[]{};
+    }
+
+
+    public static int[] twoSumsOnePass(int[] input, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int length= input.length;
+
+        for(int i=0;i<length;i++) {
+            int complement = target- input[i];
+            if(map.containsKey(complement)) {
+                return new int[]{i, map.get(complement)};
+            }
+            map.put(input[i],i);
         }
         return new int[]{};
     }
