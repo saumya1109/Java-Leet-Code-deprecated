649e2fb8dd69825f2cb8bfc5a58a3107ea14ec10
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-07-06 20:28:56 -0400

    Adding sqrt

---
+++ Sqrt/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(findSqrt(8));
+
+    }
+
+    public static int findSqrt(int x) {
+        if (x == 0 || x == 1) return x;
+
+        int left = 0;
+        int right = x;
+
+        while (left <=right) {
+            int mid = left + (right - left) / 2;
+
+            if (mid > x / mid) {
+                right = mid - 1;
+            } else if (mid < x / mid) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return right;
+    }
+}
